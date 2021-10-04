6ed4d17083c8a61f34b0df6497a8487aae6b1998
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-10-03 20:38:28 -0500

    added death valley file

--- sitka4.py
+++ sitka4.py
@@ -2,13 +2,13 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file,delimiter=',')
 
 header_row = next(csv_file)
 
-print(type(header_row))
+#print(type(header_row))
 
 for index, column_header in enumerate(header_row):
     print(index, column_header)
@@ -16,7 +16,7 @@ for index, column_header in enumerate(header_row):
 
 #testing to convert date from string
 mydate = datetime.strptime('2018-07-01', '%Y-%m-%d')
-print(type(mydate))
+#print(type(mydate))
 
 
 
@@ -24,33 +24,35 @@ highs = []
 dates = []
 lows = []
 
-for row in csv_file:
-    highs.append(int(row[5]))
-    lows.append(int(row[6]))
-    the_date = datetime.strptime(row[2],'%Y-%m-%d')
-    dates.append(the_date)
-    
+for rec in csv_file:
+    try:
+        the_date = datetime.strptime(rec[2],'%Y-%m-%d')
+        high = int(rec[4])
+        low = int(rec[5])
+        
+    except ValueError:
+        print(f'Missing data for {the_date}')
+        #f string = lets you put the variable name in the print statement
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(the_date)
 
-
-
-print(highs)
-print(dates)
+#print(highs)
+#print(dates)
 
 
 fig = plt.figure()
-
-
-plt.title("Daily high temperature,July 2018", fontsize = 16)
+plt.title("Daily highand low temperature - 2018\nDeath Valley", fontsize = 16)
 plt.xlabel("", fontsize = 12)
 plt.ylabel("Temperature (F)", fontsize = 12)
 plt.tick_params(axis="both",which = "major", labelsize = 12)
+fig.autofmt_xdate()
 
 plt.plot(dates,highs,c="red",alpha=0.5)
 plt.plot(dates,lows, c="blue", alpha=0.5)
 
 plt.fill_between(dates,highs,lows, facecolor="blue", alpha = 0.1)
-
-fig.autofmt_xdate()
 
 plt.show()
 
