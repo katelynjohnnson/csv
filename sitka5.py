bbc3935d9736970b162d0b1af1d8a095bb4ebb99
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-10-11 18:30:24 -0500

    sitka + dv comparison create

--- sitka5.py
+++ sitka5.py
@@ -3,10 +3,13 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 open_file = open("death_valley_2018_simple.csv", "r")
+open_file1 = open("sitka_weather_2018_simple.csv",'r')
 
 csv_file = csv.reader(open_file,delimiter=',')
+csv_file1 = csv.reader(open_file1,delimiter=',')
 
 header_row = next(csv_file)
+header_row = next(csv_file1)
 
 #print(type(header_row))
 
@@ -40,30 +43,39 @@ for rec in csv_file:
 
 #print(highs)
 #print(dates)
+highs1 = []
+lows1 = []
+dates1 = []
 
+for rec in csv_file1:
+    try:
+        the_date = datetime.strptime(rec[2],'%Y-%m-%d')
+        high = int(rec[5])
+        low = int(rec[6])
+        
+    except ValueError:
+        print(f'Missing data for {the_date}')
+        #f string = lets you put the variable name in the print statement
+    else:
+        highs1.append(high)
+        lows1.append(low)
+        dates1.append(the_date)
 
 fig = plt.figure()
-plt.title("Daily high and low temperature - 2018\nDeath Valley", fontsize = 16)
-plt.xlabel("", fontsize = 12)
-plt.ylabel("Temperature (F)", fontsize = 12)
-plt.tick_params(axis="both",which = "major", labelsize = 12)
-fig.autofmt_xdate()
 
-plt.plot(dates,highs,c="red",alpha=0.5)
-plt.plot(dates,lows, c="blue", alpha=0.5)
-
-plt.fill_between(dates,highs,lows, facecolor="blue", alpha = 0.1)
-
-plt.show()
 
 plt.subplot(2,1,1)
-plt.plot(dates,highs,c='red')
-plt.title('Highs')
+plt.plot(dates1,highs1,c='red')
+plt.plot(dates1,lows1,c='blue')
+plt.fill_between(dates1, highs1,lows1,facecolor='blue', alpha=0.1)
+plt.title('SITKA AIRPORT, AK US')
 
 plt.subplot(2,1,2)
+plt.plot(dates,highs,c='red')
 plt.plot(dates,lows,c='blue')
-plt.title("Lows")
+plt.fill_between(dates, highs,lows,facecolor='blue', alpha=0.1)
+plt.title("DEATH VALLEY, CA US")
 
-plt.suptitle("Highs and Lows of Sitka, Alaska")
+plt.suptitle("Temperature comparison between SITKA AIRPORT, US AK and DEATH VALLEY, CA US")
 
 plt.show()
